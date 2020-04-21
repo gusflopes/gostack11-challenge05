77da7f3d5d059282ebd510d5c0f17da9c9025c12
gusflopes <gusflopes86@gmail.com>
2020-04-21 14:05:36 -0400

    feat: challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,52 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    // console.log(this.transactions);
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  private updateBalance(transaction: Transaction): void {
+    if (transaction.type === 'income') {
+      this.balance.income += transaction.value;
+    }
+    if (transaction.type === 'outcome') {
+      this.balance.outcome += transaction.value;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+  }
+
+  public create(data: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction(data);
+    this.transactions.push(transaction);
+    if (
+      transaction.type === 'outcome' &&
+      this.balance.total - transaction.value < 0
+    ) {
+      throw Error('Sem grana companheiro!');
+    }
+    this.updateBalance(transaction);
+    return transaction;
   }
 }
 
